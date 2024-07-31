8618a84195dbedd394ba10d49a0517f40a5ad8a2
HThien02 <hieuthien2k2@gmail.com>
2024-07-31 16:05:32 +0700

    Config ReactJS + Vite, Tailwindcss + Datatables DONE

---
+++ TUTORME_UI_V1/src/components/DataTable.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import 'datatables.net-dt/css/dataTables.dataTables.min.css';
+import $ from 'jquery';
+import 'datatables.net';
+
+const DataTable: React.FC = () => {
+  useEffect(() => {
+    $('#example').DataTable();
+  }, []);
+
+  return (
+    <div className="p-4">
+      <table id="example" className="display w-full text-center">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Tiger Nixon</td>
+            <td>System Architect</td>
+            <td>Edinburgh</td>
+            <td>61</td>
+            <td>2011/04/25</td>
+            <td>$320,800</td>
+          </tr>
+          <tr>
+            <td>Garrett Winters</td>
+            <td>Accountant</td>
+            <td>Tokyo</td>
+            <td>63</td>
+            <td>2011/07/25</td>
+            <td>$170,750</td>
+          </tr>
+          {/* Add more rows as needed */}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DataTable;

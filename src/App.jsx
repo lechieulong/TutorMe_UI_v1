99745962f24c8f53138ca2fabdb5657f7f282d49
lechieulong <lechieulong2016@gmail.com>
2024-08-02 22:55:54 +0700

    TestView

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import DataTable from './components/DataTable';
+import Test from './pages/TestExam/Test';
 
 const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full">
-        <h1 className="text-2xl font-bold text-gray-900">Hello, Tailwind CSS!</h1>
-        <p className="mt-4 text-gray-600">This is a simple test to check if Tailwind CSS is working properly.</p>
-        <DataTable />
+      <div className="bg-white rounded-lg shadow-lg w-full">
+        <Test/>
       </div>
     </div>
   );
---
+++ src/components/Test/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className=" left-0 right-0 flex justify-between p-4  fixed bg-black shadow-md ">
+      <h3 className="text-lg font-semibold">Logo</h3>
+      <h3 className="text-lg font-semibold">Time</h3>
+      <h3 className="text-lg font-semibold">Setting</h3>
+    </div>
+  );
+};
+
+export default Header;

e495c6170f4b5defb8c152d31b10c4cf5d27e294
lechieulong <98459887+lechieulong@users.noreply.github.com>
2024-08-12 21:08:09 +0700

    CI/CD

--- src/components/Test/Question.jsx
+++ src/components/Test/Question.jsx
@@ -1,31 +1,39 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+const Question = ({ part, questionsByType }) => {
+  const subjectType = Object.freeze({
+    READING: 0,
+    LISTENING: 1,
+    WRITING: 2,
+    SPEAKING: 3,
+    OTHER: 4,
+  });
 
-const Question = () => {
+  let contentToDisplay = "";
+
+  switch (part) {
+    case subjectType.READING:
+      contentToDisplay = questionsByType["reading"]?.content;
+      break;
+    case subjectType.LISTENING:
+      contentToDisplay = questionsByType["listening"]?.content;
+      break;
+    case subjectType.WRITING:
+      contentToDisplay = questionsByType["writing"]?.content;
+      break;
+    case subjectType.SPEAKING:
+      contentToDisplay = questionsByType["speaking"]?.content;
+      break;
+    case subjectType.OTHER:
+      contentToDisplay = questionsByType["other"]?.content;
+      break;
+    default:
+      contentToDisplay = "No content available";
+      break;
+  }
+
   return (
     <div className="p-5">
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero ullam non quidem, sequi voluptates magnam esse delectus harum saepe, vel quasi ducimus molestiae iure, modi explicabo deserunt id blanditiis eveniet?
-        Sint molestiae commodi odit enim praesentium. Saepe obcaecati hic consequuntur corrupti, sequi expedita blanditiis ducimus minima, facilis omnis nobis voluptatum quam doloribus non sed ad? Nulla tenetur dolorum omnis perspiciatis.
-        Sed similique, consequuntur maiores dolorem eaque laborum. Quidem et, amet inventore velit architecto repellat voluptas consectetur nisi eum fugiat eveniet magni vel sequi ipsum illo! Inventore incidunt quos placeat dolorem.
-        Nemo, commodi odit. Beatae veritatis labore laboriosam minima non neque recusandae voluptates earum nam fuga accusantium eaque, ratione quisquam quis facere at natus rem voluptate omnis modi quam. Corporis, obcaecati.
-        Tenetur quam nam ea eum doloremque facere repellat assumenda esse eaque explicabo. Porro, sapiente! Itaque quod soluta unde, minus voluptates eligendi tempore qui illo dolor veniam eum voluptate, optio delectus.
-        Quia temporibus assumenda qui itaque nam! Architecto accusantium obcaecati neque aliquid odio? Veritatis pariatur maiores debitis accusantium laborum mollitia ab, maxime sint dolores repudiandae officiis dicta, aliquid praesentium voluptates hic?
-        Perspiciatis minus ea non maxime expedita accusamus, pariatur facilis aperiam eveniet, placeat corrupti, facere at nulla veniam consequatur. Sit recusandae saepe tempora voluptatibus! Voluptatibus animi ea corrupti et molestiae possimus?
-        Explicabo enim veritatis et illo doloribus numquam aperiam exercitationem, incidunt quae ab sapiente voluptates soluta? Molestiae numquam quae neque sit labore quibusdam laudantium libero sed ut, ipsam, incidunt ea aliquid.
-        Cupiditate in quasi placeat ipsam assumenda fuga voluptatem voluptas reprehenderit tempora quis, nam eum unde ducimus quam sapiente consequuntur deleniti facere accusantium consectetur veritatis provident accusamus! Adipisci, minima? Blanditiis, aut.
-        Sint voluptatibus ut sapiente unde blanditiis deleniti ratione. Dolorem, provident deserunt odit beatae illum sit? Cumque, dolor. Doloremque, sint harum. Numquam ullam accusantium consectetur enim quas assumenda eum modi nihil.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero ullam non quidem, sequi voluptates magnam esse delectus harum saepe, vel quasi ducimus molestiae iure, modi explicabo deserunt id blanditiis eveniet?
-        Sint molestiae commodi odit enim praesentium. Saepe obcaecati hic consequuntur corrupti, sequi expedita blanditiis ducimus minima, facilis omnis nobis voluptatum quam doloribus non sed ad? Nulla tenetur dolorum omnis perspiciatis.
-        Sed similique, consequuntur maiores dolorem eaque laborum. Quidem et, amet inventore velit architecto repellat voluptas consectetur nisi eum fugiat eveniet magni vel sequi ipsum illo! Inventore incidunt quos placeat dolorem.
-        Nemo, commodi odit. Beatae veritatis labore laboriosam minima non neque recusandae voluptates earum nam fuga accusantium eaque, ratione quisquam quis facere at natus rem voluptate omnis modi quam. Corporis, obcaecati.
-        Tenetur quam nam ea eum doloremque facere repellat assumenda esse eaque explicabo. Porro, sapiente! Itaque quod soluta unde, minus voluptates eligendi tempore qui illo dolor veniam eum voluptate, optio delectus.
-        Quia temporibus assumenda qui itaque nam! Architecto accusantium obcaecati neque aliquid odio? Veritatis pariatur maiores debitis accusantium laborum mollitia ab, maxime sint dolores repudiandae officiis dicta, aliquid praesentium voluptates hic?
-        Perspiciatis minus ea non maxime expedita accusamus, pariatur facilis aperiam eveniet, placeat corrupti, facere at nulla veniam consequatur. Sit recusandae saepe tempora voluptatibus! Voluptatibus animi ea corrupti et molestiae possimus?
-        Explicabo enim veritatis et illo doloribus numquam aperiam exercitationem, incidunt quae ab sapiente voluptates soluta? Molestiae numquam quae neque sit labore quibusdam laudantium libero sed ut, ipsam, incidunt ea aliquid.
-        Cupiditate in quasi placeat ipsam assumenda fuga voluptatem voluptas reprehenderit tempora quis, nam eum unde ducimus quam sapiente consequuntur deleniti facere accusantium consectetur veritatis provident accusamus! Adipisci, minima? Blanditiis, aut.
-        Sint voluptatibus ut sapiente unde blanditiis deleniti ratione. Dolorem, provident deserunt odit beatae illum sit? Cumque, dolor. Doloremque, sint harum. Numquam ullam accusantium consectetur enim quas assumenda eum modi nihil.
-   
-      </p>
+      <p>{contentToDisplay}</p>
     </div>
   );
 };

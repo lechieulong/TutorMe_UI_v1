fa8bd71e09424fcb33309f46c779da349add3a92
HThien02 <hieuthien2k2@gmail.com>
2024-09-05 12:17:29 +0700

    Done UI Course

--- src/components/Mentor/MentorSideBar.jsx
+++ src/components/Mentor/MentorSideBar.jsx
@@ -30,7 +30,7 @@ const MentorSidebar = () => {
                   <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
                   <polyline points="9 22 9 12 15 12 15 22" />
                 </svg>
-                Dashboard
+                Classes
               </a>
             </li>
 
@@ -56,40 +56,7 @@ const MentorSidebar = () => {
                   <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
                   <path d="M16 3.13a4 4 0 0 1 0 7.75" />
                 </svg>
-                Users
-              </a>
-            </li>
-
-            <li>
-              <a
-                className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                href="#"
-              >
-                <svg
-                  className="size-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <circle cx="18" cy="15" r="3" />
-                  <circle cx="9" cy="7" r="4" />
-                  <path d="M10 15H6a4 4 0 0 0-4 4v2" />
-                  <path d="m21.7 16.4-.9-.3" />
-                  <path d="m15.2 13.9-.9-.3" />
-                  <path d="m16.6 18.7.3-.9" />
-                  <path d="m19.1 12.2.3-.9" />
-                  <path d="m19.6 18.7-.4-1" />
-                  <path d="m16.8 12.3-.4-1" />
-                  <path d="m14.3 16.6 1-.4" />
-                  <path d="m20.7 13.8 1-.4" />
-                </svg>
-                Account
+                Livestream
               </a>
             </li>
 
@@ -114,7 +81,7 @@ const MentorSidebar = () => {
                   <path d="M3 7.6v12.8c0 .4.2.8.5 1.1.3.3.7.5 1.1.5h9.8" />
                   <path d="M15 2v5h5" />
                 </svg>
-                Projects
+                Test
               </a>
             </li>
 
@@ -169,7 +136,7 @@ const MentorSidebar = () => {
                   <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
                   <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
                 </svg>
-                Documentation
+                Report
               </a>
             </li>
           </ul>

bbd995051f425ab6cb4155fd608952e134daefd9
lechieulong <lechieulong2016@gmail.com>
2024-09-06 12:59:14 +0700

    install dotenv

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyAc-cSMsNThP_OrOvUXkrr9_vbo7rn_dJw",
-  authDomain: "tutorme-cf260.firebaseapp.com",
-  projectId: "tutorme-cf260",
-  storageBucket: "tutorme-cf260.appspot.com",
-  messagingSenderId: "494374119554",
-  appId: "1:494374119554:web:5d1a80c1a96c0338dd2c9b",
-  measurementId: "G-DMEWEVM08W"
+// Your web app's Firebase configuration from environment variables
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase

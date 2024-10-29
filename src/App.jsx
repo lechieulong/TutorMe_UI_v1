6fb881c8f8f1c0cc3dcb4e81affcee293b4fe6a0
lechieulong <lechieulong2016@gmail.com>
2024-10-29 16:08:56 +0700

    handle create test for specific skill and admin

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ const App = () => {
           <Route path="/listTest" element={<ListTest />} />
           <Route path="/create-skill" element={<CreateTest />} />
           <Route path="/create-test/:sectionCourseId" element={<TestForm />} />
-          <Route path="/create-test" element={<CreateTestLayout />} />
+          <Route path="/create-test" element={<TestForm />} />
           <Route path="/questionbank" element={<QuestionBank />} />
 
           {/* LiveStream  exam*/}

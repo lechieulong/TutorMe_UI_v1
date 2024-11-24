d8516b5cf73fb77bf055b0a7760062b439867f51
lechieulong <98459887+lechieulong@users.noreply.github.com>
2024-11-24 19:32:43 +0700

    createLessonTest

--- src/pages/Mentor/component/CourseLessonCard.jsx
+++ src/pages/Mentor/component/CourseLessonCard.jsx
@@ -4,6 +4,7 @@ import Cookies from "js-cookie";
 import CreateCourseLessonContent from "./CreateCourseLessonContent";
 import CourseLessonContent from "./CourseLessonContent";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+import TestForm from "../../ExamTest/TestForm";
 
 const CourseLessonCard = ({
   mentorAndList,
@@ -14,6 +15,9 @@ const CourseLessonCard = ({
   const [collapsedLessons, setCollapsedLessons] = useState({});
   const [courseLessons, setCourseLessons] = useState([]);
   const [lessionCount, setLessionCount] = useState(null);
+  const [isCreateTest, setIsCreateTest] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [lessonId, setLessonId] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [dynamicForms, setDynamicForms] = useState([]);
@@ -73,12 +77,14 @@ const CourseLessonCard = ({
 
   const handleCreateTest = async (lessonId) => {
     try {
-      console.log("Lesson ID:", lessonId);
       const response = await axios.get(
         `https://localhost:7030/api/CourseSkills/DescriptionByCourseLesson/${lessonId}`,
         { headers: { Authorization: `Bearer ${token}` } }
       );
       console.log("API Response Data:", response.data);
+      setIsCreateTest(true);
+      setCategories([response.data]);
+      setLessonId(lessonId);
     } catch (error) {
       console.error("Failed to fetch data from API", error);
     }
@@ -93,69 +99,80 @@ const CourseLessonCard = ({
   }
 
   return (
-    <div className="h-vh100 px-4 mb-4 relative">
-      <div className="cursor-pointer">
-        <h3 className="text-sm font-semibold text-gray-400">
-          {courseLessons.length} Lessons
-        </h3>
-      </div>
+    <>
+      {!isCreateTest ? (
+        <>
+          {" "}
+          <div className="h-vh100 px-4 mb-4 relative">
+            <div className="cursor-pointer">
+              <h3 className="text-sm font-semibold text-gray-400">
+                {courseLessons.length} Lessons
+              </h3>
+            </div>
 
-      <div className="mt-4 overflow-hidden transition-all duration-1000 ease-in-out">
-        <div>
-          {courseLessons.length === 0 ? (
-            <p>No lessons found for this course part.</p>
-          ) : (
-            courseLessons.map((courseLesson) => (
-              <div
-                key={courseLesson.id}
-                className="border rounded-md p-2 mb-2 shadow-sm flex flex-col items-start"
-              >
-                <div
-                  className="cursor-pointer flex items-center gap-2"
-                  onClick={() => toggleCollapse(courseLesson.id)}
-                >
-                  {collapsedLessons[courseLesson.id] ? (
-                    <FaAngleDown className="text-gray-600" />
-                  ) : (
-                    <FaAngleUp className="text-gray-600" />
-                  )}
-                  <h4 className="text-md font-semibold">
-                    {courseLesson.title}
-                  </h4>
-                </div>
-                <button
-                  type="button"
-                  onClick={() => handleCreateTest(courseLesson.id)}
-                  className="py-2 px-3 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50"
-                >
-                  Create Test
-                </button>
-                {!collapsedLessons[courseLesson.id] && (
-                  <>
-                    {mentorAndList &&
-                      dynamicForms
-                        .filter((form) => form.lessonId === courseLesson.id)
-                        .map((form) => (
-                          <div key={form.id} className="mt-2 w-full">
-                            <CreateCourseLessonContent
-                              courseLessonId={courseLesson.id}
-                              onClose={() => removeDynamicForm(form.id)}
-                            />
-                          </div>
-                        ))}
-
-                    <CourseLessonContent
-                      courseLessontId={courseLesson.id}
+            <div className="mt-4 overflow-hidden transition-all duration-1000 ease-in-out">
+              <div>
+                {courseLessons.length === 0 ? (
+                  <p>No lessons found for this course part.</p>
+                ) : (
+                  courseLessons.map((courseLesson) => (
+                    <div
                       key={courseLesson.id}
-                    />
-                  </>
+                      className="border rounded-md p-2 mb-2 shadow-sm flex flex-col items-start"
+                    >
+                      <div
+                        className="cursor-pointer flex items-center gap-2"
+                        onClick={() => toggleCollapse(courseLesson.id)}
+                      >
+                        {collapsedLessons[courseLesson.id] ? (
+                          <FaAngleDown className="text-gray-600" />
+                        ) : (
+                          <FaAngleUp className="text-gray-600" />
+                        )}
+                        <h4 className="text-md font-semibold">
+                          {courseLesson.title}
+                        </h4>
+                      </div>
+                      <button
+                        type="button"
+                        onClick={() => handleCreateTest(courseLesson.id)}
+                        className="py-2 px-3 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50"
+                      >
+                        Create Test
+                      </button>
+                      {!collapsedLessons[courseLesson.id] && (
+                        <>
+                          {mentorAndList &&
+                            dynamicForms
+                              .filter(
+                                (form) => form.lessonId === courseLesson.id
+                              )
+                              .map((form) => (
+                                <div key={form.id} className="mt-2 w-full">
+                                  <CreateCourseLessonContent
+                                    courseLessonId={courseLesson.id}
+                                    onClose={() => removeDynamicForm(form.id)}
+                                  />
+                                </div>
+                              ))}
+
+                          <CourseLessonContent
+                            courseLessontId={courseLesson.id}
+                            key={courseLesson.id}
+                          />
+                        </>
+                      )}
+                    </div>
+                  ))
                 )}
               </div>
-            ))
-          )}
-        </div>
-      </div>
-    </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <TestForm lessonId={lessonId} categories={categories} />
+      )}
+    </>
   );
 };
 

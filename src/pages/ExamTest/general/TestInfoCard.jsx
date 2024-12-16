69f9fd6c3382e32aa231204af6642bd37d58723b
lechieulong <98459887+lechieulong@users.noreply.github.com>
2024-12-16 23:25:30 +0700

    fix bug

--- src/pages/ExamTest/general/TestInfoCard.jsx
+++ src/pages/ExamTest/general/TestInfoCard.jsx
@@ -1,36 +1,60 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlusCircle,
+  faInfoCircle,
+  faClock,
+  faCalendarAlt,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 const TestInfoCard = ({ testInfo, setShowTestFormDetail }) => {
   return (
-    <div className="p-6 max-w-lg mx-auto bg-white shadow-lg rounded-xl border border-gray-200">
-      <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+    <div className="p-6 max-w-lg mx-auto bg-white shadow-lg rounded-xl border border-gray-200 transition duration-300 hover:shadow-2xl hover:border-gray-300">
+      <h2 className="text-3xl font-bold mb-10 text-gray-800 text-center">
+        <FontAwesomeIcon icon={faInfoCircle} className="mr-2 text-green-800" />{" "}
         Test Information
       </h2>
 
-      <p className="mb-2 text-gray-700">
-        <strong className="text-gray-900">Test Name:</strong>{" "}
-        {testInfo.testName || "N/A"}
-      </p>
+      <div className="space-y-4">
+        <p className="text-gray-700 flex items-center">
+          <FontAwesomeIcon
+            icon={faCalendarAlt}
+            className="mr-2 text-green-800"
+          />
+          <p className="text-gray-900 font-bold">Test Name:</p>{" "}
+          <span className="ml-6">{testInfo.testName || "N/A"}</span>
+        </p>
 
-      <p className="mb-2 text-gray-700">
-        <strong className="text-gray-900">Start Time:</strong>{" "}
-        {testInfo.startTime || "N/A"}
-      </p>
+        <p className="text-gray-700 flex items-center">
+          <FontAwesomeIcon icon={faClock} className="mr-2 text-green-800" />
+          <strong className="text-gray-900">Start Time:</strong>{" "}
+          <span className="ml-6">{testInfo.startTime || "N/A"}</span>
+        </p>
 
-      <p className="mb-4 text-gray-700">
-        <strong className="text-gray-900">End Time:</strong>{" "}
-        {testInfo.endTime || "N/A"}
-      </p>
-
-      <button
-        type="button"
-        onClick={() => setShowTestFormDetail(true)}
-        className="mt-4 flex items-center bg-green-600 hover:bg-green-700 text-white py-2 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
-      >
-        <FontAwesomeIcon icon={faPlusCircle} className="mr-2" />
-        Create Skill
-      </button>
+        <p className="text-gray-700 flex items-center">
+          <FontAwesomeIcon icon={faClock} className="mr-2 text-green-800" />
+          <strong className="text-gray-900">End Time:</strong>{" "}
+          <span className="ml-6">{testInfo.endTime || "N/A"}</span>
+        </p>
+      </div>
+      <div className="flex space-x-4 mt-6">
+        <button
+          type="button"
+          onClick={() => setShowTestFormDetail(true)}
+          className="flex items-center justify-center bg-green-600 hover:bg-green-700 text-white font-medium py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+        >
+          <FontAwesomeIcon icon={faPlusCircle} className="mr-2 text-lg" />
+          Create Skill
+        </button>
+        <button
+          type="button"
+          className="flex items-center justify-center bg-red-50 text-red-600 font-medium py-3 px-6 rounded-lg shadow-md hover:bg-red-100 transition duration-300 ease-in-out transform hover:scale-105"
+          onClick={() => window.location.reload()}
+        >
+          <FontAwesomeIcon icon={faTimesCircle} className="mr-2 text-lg" />
+          Close
+        </button>
+      </div>
     </div>
   );
 };

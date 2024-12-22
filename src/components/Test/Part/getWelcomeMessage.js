85412d8e871761d15f2805e12f41d43cd4f69df3
lechieulong <98459887+lechieulong@users.noreply.github.com>
2024-12-23 06:18:36 +0700

    merge deploy final

--- src/components/Test/Part/getWelcomeMessage.js
+++ src/components/Test/Part/getWelcomeMessage.js
@@ -3,7 +3,7 @@ export const getWelcomeMessage = (partNumber) => {
     case 1:
       // return "";
       return `Hello, and welcome to the IELTS Speaking test. I will be your examiner today. 
-           The Speaking test is divided into three parts. I will explain each part as we go along, and I will ask you to speak on a variety of topics
+           The Speaking test is divided into three parts. I will explain each part as we go along, and I will ask you to speak on a variety of topics,
        Part 1;`;
     case 2:
       return `In Part 2, I will give you a task card with a topic and prompts. You will have 1 minute to prepare, and then I would like you to speak for 1-2 minutes on the topic.`;
